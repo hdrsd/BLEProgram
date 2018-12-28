fed37280bf8ffbb88b4ad4e53ecb33b35d830244
hdrsd <lastrsd@gmail.com>
2018-12-28 21:32:31 +0900

    TEST_UNKNOWN

--- BLEProgram/NrfUuid.cs
+++ BLEProgram/NrfUuid.cs
@@ -17,5 +17,7 @@ namespace BLEProgram
         public static Guid RX_SERVICE_UUID = Guid.Parse("6e400001-b5a3-f393-e0a9-e50e24dcca9e");
         public static Guid RX_CHAR_UUID = Guid.Parse("6e400002-b5a3-f393-e0a9-e50e24dcca9e");
         public static Guid TX_CHAR_UUID = Guid.Parse("6e400003-b5a3-f393-e0a9-e50e24dcca9e");
+        public static Guid UNKNOWN_SERVICE_UUID = Guid.Parse("0000beef-1212-efde-1523-785fef13d123");
+        public static Guid UNKNOWN_CHAR_UUID = Guid.Parse("0000beef-1212-efde-1523-785fef13d123");
     }
 }
